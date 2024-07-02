6928a7e9e6c2da9fc2bc73af1c7c503a6b00cead
Iago Carvalho Guimaraes <iago.00000845691@unicap.br>
2024-07-02 11:43:28 -0300

    iniciado

---
+++ assignment-board-api/src/main/java/com/projetospessoal/assignmentboard/AssignmentBoardApiApplication.java
@@ -0,0 +1,13 @@
+package com.projetospessoal.assignmentboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentBoardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentBoardApiApplication.class, args);
+	}
+
+}
---
+++ assignment-board-api/src/test/java/com/projetospessoal/assignmentboard/AssignmentBoardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetospessoal.assignmentboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentBoardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
